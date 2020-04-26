118eb2d4d66ac6edf886d2a9b21540d0851df1fe
paachary <prashant_acharya74@yahoo.com>
2020-04-26 19:34:06 +0530

    adding external table

--- hive/table_create_load.sql
+++ hive/table_create_load.sql
@@ -1,9 +1,27 @@
+-- Managed Table
 create table if not exists 
-movies (movie_id int, title String, genre String) 
-comment 'table holding movies data' 
+movies (movie_id int, 
+	title String, 
+	genre String) 
+comment 'managed table holding movies data' 
 row format delimited 
 fields terminated by ',' 
 lines terminated by '\n' ;
 
 load data inpath 'data/movies.csv' overwrite into table movies;
 
+
+-- External Table
+create external table if not exists
+ratings (
+	user_id int, 
+	movie_id int,
+	rating double,
+	time_stamp int )
+comment 'external table holding ratings data' 
+row format delimited 
+fields terminated by ',' lines 
+terminated by '\n' 
+location 'data' ;
+
+load data inpath 'data/ratings.csv' into table ratings;
