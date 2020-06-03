1b815ed54b0799e788b91200c99361f3a8ae2fc5
Prashant Acharya <prashant_acharya74@yahoo.com>
2020-06-03 17:59:20 +0530

    Update Redshift-Spark.scala

--- Spark/Redshift/Redshift-Spark.scala
+++ Spark/Redshift/Redshift-Spark.scala
@@ -5,11 +5,11 @@
 
  output.write.format("jdbc").
  option("driver","com.amazon.redshift.jdbc42.Driver").
- option("url","jdbc:redshift://redshift-cluster-1.cyrrz8bfvxa1.us-east-1.redshift.amazonaws.com:5439").
+ option("url","<<jdbc url>>").
  option("ConnSchema","dev").
  option("dbtable","movies").
  option("user","awsuser").
- option("password","Prax4Vats").
+ option("password","<<>>").
  option("dbtable","movies").
  option("truncate","false").
  mode(saveMode = SaveMode.Overwrite).
