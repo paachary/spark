bc54570c764ca173b12685430b8f498cadbde99a
paachary <prashant_acharya74@yahoo.com>
2020-06-03 19:58:27 +0530

    adding the hive commands for updating the metadata tables with partition information

--- hive/table_create_load.sql
+++ hive/table_create_load.sql
@@ -25,3 +25,19 @@ terminated by '\n'
 location 'data' ;
 
 load data inpath 'data/ratings.csv' into table ratings;
+
+create external table movie_lens (movie_id int, occupation string, genre string, avg_rating string)
+stored as parquet location 's3://workflow-1-bucket-2020/output/';
+
+
+CREATE DATABASE s3movielens LOCATION 's3://prax-bucket/output/';
+
+create external table s3movielens.movie_lens (movie_id int)
+partitioned by (avg_rating DECIMAL(5,2), occupation string, genre string) 
+stored as parquet location 's3://prax-bucket/output/';
+
+MSCK REPAIR TABLE s3movielens.movie_lens;
+
+
+select * from s3movielens.movie_lens_1;
+
